c958665baac79f56b2da16ec47425a8e64b52bd5
MADHESH <kulla7364@gmail.com>
2026-07-01 10:35:01 +0530

    Update greet.js

--- greet.js
+++ greet.js
@@ -1,10 +1,11 @@
 // Write a function that returns a message using template literals.
 
-function getMessage(name, age) {
-    // Write your code here
+function getMessage(name, age) { 
+    return `Hello, my name is ${name} and I am ${age} years old.`;
 }
 
 // Don't change the code below
 console.log(getMessage(process.argv[2], process.argv[3]));
+
 
 module.exports = getMessage;
